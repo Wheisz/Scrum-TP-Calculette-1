71cd6a450498c03c7fc880576b2b065b53a1999e
tsupio <t.supiot@iia-laval.fr>
2018-06-08 15:49:52 +0200

    start Add class

---
+++ src/main/java/com/scrum/calculator/add/Add.java
@@ -0,0 +1,11 @@
+package com.scrum.calculator.add;
+
+import com.scrum.calculator.core.Operation;
+
+public class Add extends Operation {
+
+    @Override
+    public float getNumberOfParameters() {
+        return super.getNumberOfParameters();
+    }
+}
