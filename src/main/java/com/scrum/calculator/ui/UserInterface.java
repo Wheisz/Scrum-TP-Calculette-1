6f3d98c38711ff2dbc8a97f15316b26f60a1e3dd
Willy DROUIN <w.drouin@iia-laval.fr>
2018-06-08 16:44:58 +0200

    Commit last lol

--- src/main/java/com/scrum/calculator/ui/UserInterface.java
+++ src/main/java/com/scrum/calculator/ui/UserInterface.java
@@ -1,5 +1,20 @@
 package com.scrum.calculator.ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
+	private static Scanner reader = new Scanner(System.in);
+	
+	public static void displayMessage(String message){
+		System.out.println(message);
+	}
+	
+	public static int getIntFromUser(){
+		return reader.nextInt();
+	}
+	
+	public static float getFloatFromUser(){
+		return reader.nextFloat();
+	}
 }
