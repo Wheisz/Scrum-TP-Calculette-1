083f0d48ff735d17a0f0dbd1c5a693e09d8ccd33
tsupio <t.supiot@iia-laval.fr>
2018-06-08 16:41:20 +0200

    doc Operation absract class

--- src/main/java/com/scrum/calculator/core/Operation.java
+++ src/main/java/com/scrum/calculator/core/Operation.java
@@ -2,12 +2,28 @@ package com.scrum.calculator.core;
 
 import java.util.ArrayList;
 
+/**
+ * Abstract Operation class.
+ * @author willy drouin
+ */
 public abstract class Operation {
 
+	/**
+	 * Number of parameters necessary to do the operation.
+	 */
 	protected int numberOfParameters;
-		
+
+	/**
+	 * Method to execute operation.
+	 * @param listNumber list of values to operate.
+	 * @return result of operation.
+	 */
 	public abstract float execute(ArrayList<Float> listNumber);
-	
+
+	/**
+	 * Getter number of parameters for operation.
+	 * @return
+	 */
 	public int getNumberOfParameters(){
 		return this.numberOfParameters;
 	}
