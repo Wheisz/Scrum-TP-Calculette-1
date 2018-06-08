2813b464ec14ccb4680ab9e77e3315e1b0b41212
tsupio <t.supiot@iia-laval.fr>
2018-06-08 16:26:56 +0200

    doc opration_add

--- src/main/java/com/scrum/calculator/add/Add.java
+++ src/main/java/com/scrum/calculator/add/Add.java
@@ -5,18 +5,23 @@ import com.scrum.calculator.core.Operation;
 import java.util.ArrayList;
 
 /**
- * Addition
+ * Addition.
  * @author tsupio
  */
 public class Add extends Operation {
 
     /**
-     * Constructor
+     * Constructor.
      */
     public Add(){
         super.numberOfParameters = 2;
     }
 
+    /**
+     * Execute Add operation.
+     * @param listNumber list of values to add
+     * @return result of addition
+     */
     @Override
     public float execute(final ArrayList<Float> listNumber) {
         float result = 0;
@@ -28,6 +33,10 @@ public class Add extends Operation {
         return result;
     }
 
+    /**
+     * Number of values to add.
+     * @return
+     */
     @Override
     public int getNumberOfParameters() {
         return super.getNumberOfParameters();
